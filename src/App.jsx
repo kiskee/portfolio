26220b7a1aa380acac56cf64d119f08729570286
danielmed <daniel.medina@talent.com>
2023-05-10 23:13:41 -0500

    adding the first steps on my own portfolio

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+
+
+  return (
+    <>
+   <header className="w-full text-slate-50 bg-black border-gray-100 shadow-sm body-font">
+        <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+
+            <nav className="flex flex-wrap items-center text-base lg:w-2/5 md:ml-auto">
+                <a href="#_" className="mr-5 font-medium hover:text-gray-900" target="_blank">Home</a>
+                <a href="#_" className="mr-5 font-medium hover:text-gray-900" target="_blank">About</a>
+                <a href="#_" className="font-medium hover:text-gray-900" target="_blank">Contact</a>
+            </nav>
+            <svg className="w-auto h-5 text-gray-900 fill-current" viewBox="-1 -1 488 42" xmlns="http://www.w3.org/2000/svg"><g id="fontsvg1683778216743" stroke="#d466e7"  fill="#000000"><path d="M 270.4 20 L 281.92 0 L 299.24 30 L 293.48 40 L 281.92 20 L 276.16 30 L 281.92 40 L 270.4 40 L 258.84 20 L 253.08 30 L 258.84 40 L 247.28 40 L 241.52 30 L 258.84 0 L 270.4 20 Z M 63.48 10 L 46.16 40 L 40.4 30 L 57.72 0 L 80.8 0 L 86.6 10 L 69.28 40 L 57.72 40 L 63.48 30 L 57.72 20 L 69.28 20 L 75.04 10 L 63.48 10 Z M 462.24 10 L 444.92 40 L 439.16 30 L 456.48 0 L 479.56 0 L 485.36 10 L 468.04 40 L 456.48 40 L 462.24 30 L 456.48 20 L 468.04 20 L 473.8 10 L 462.24 10 Z M 150.08 30 L 161.6 30 L 167.4 40 L 144.28 40 L 138.52 30 L 155.84 0 L 178.92 0 L 184.72 10 L 161.6 10 L 158.72 15 L 170.28 15 L 164.48 25 L 152.96 25 L 150.08 30 Z M 312.2 30 L 323.72 30 L 329.52 40 L 306.4 40 L 300.64 30 L 317.96 0 L 341.04 0 L 346.84 10 L 323.72 10 L 320.84 15 L 332.4 15 L 326.6 25 L 315.08 25 L 312.2 30 Z M 121.2 0 L 127 10 L 109.68 40 L 98.12 20 L 92.36 30 L 98.12 40 L 86.56 40 L 80.8 30 L 98.12 0 L 109.68 20 L 121.2 0 Z M 439.16 0 L 444.96 10 L 427.64 40 L 416.08 20 L 410.32 30 L 416.08 40 L 404.52 40 L 398.76 30 L 416.08 0 L 427.64 20 L 439.16 0 Z M 5.76 40 L 0 30 L 23.08 30 L 34.64 10 L 11.56 10 L 17.32 0 L 40.4 0 L 46.2 10 L 28.88 40 L 5.76 40 Z M 346.8 40 L 341.04 30 L 364.12 30 L 375.68 10 L 352.6 10 L 358.36 0 L 381.44 0 L 387.24 10 L 369.92 40 L 346.8 40 Z M 178.92 30 L 196.24 0 L 207.8 0 L 190.48 30 L 213.56 30 L 207.8 40 L 184.68 40 L 178.92 30 Z M 138.52 0 L 144.28 10 L 132.76 30 L 138.52 40 L 126.96 40 L 121.2 30 L 138.52 0 Z M 398.76 0 L 404.52 10 L 393 30 L 398.76 40 L 387.2 40 L 381.44 30 L 398.76 0 Z" /></g></svg>
+            <div className="inline-flex items-center h-full ml-5 lg:w-2/5 lg:justify-end lg:ml-0">
+                <a href="#_" className="mr-5 font-medium hover:text-gray-900" target="_blank">Login</a>
+                <a href="#_" className="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-purple-500 rounded shadow outline-none active:bg-teal-600 hover:shadow-md hover:bg-teal-600 focus:outline-none ease" target="_blank">
+                    Sign Up
+                </a>
+            </div>
+        </div>
+    </header>
+    
+    </>
+  )
+}
+
+export default App
