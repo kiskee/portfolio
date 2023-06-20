1f563564e9d80c0052d6bbf9269cbfc391b3ec2a
danielmed <daniel.medina@talent.com>
2023-06-20 14:53:46 -0500

    adding the react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,15 @@
-import logo from './assets/final.png'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import NoPage from './Pages/NoPage'
 
 function App () {
   return (
-    <>
-      <header className='w-full text-slate-50 bg-black border-gray-100 shadow-sm body-font font-MyFont'>
-        <div className='container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row'>
-          <nav className='flex flex-wrap items-center text-base lg:w-2/5 md:ml-auto'>
-            <a
-              href='#_'
-              className='mr-5 font-medium hover:text-gray-900'
-              target='_blank'
-            >
-              Home
-            </a>
-            <a
-              href='#_'
-              className='mr-5 font-medium hover:text-gray-900'
-              target='_blank'
-            >
-              About
-            </a>
-            <a
-              href='#_'
-              className='font-medium hover:text-gray-900'
-              target='_blank'
-            >
-              Contact
-            </a>
-          </nav>
-          <img  src={logo} alt='logo' height={200} width={200}/>
-          <div className='inline-flex items-center h-full ml-5 lg:w-2/5 lg:justify-end lg:ml-0'>
-            <a
-              href='#_'
-              className='mr-5 font-medium hover:text-gray-900'
-              target='_blank'
-            >
-              Login
-            </a>
-            <a
-              href='#_'
-              className='px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-purple-500 rounded shadow outline-none active:bg-teal-600 hover:shadow-md hover:bg-teal-600 focus:outline-none ease'
-              target='_blank'
-            >
-              Sign Up
-            </a>
-          </div>
-        </div>
-      </header>
-      <div className='burbujas'>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-        <div className='burbuja'></div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Pages/NoPage.jsx
@@ -0,0 +1,10 @@
+const NoPage = () => {
+  return (
+    <div className="NoPage">
+        <h1>No Ereror 404</h1>
+    </div>
+    
+  )
+}
+
+export default NoPage
